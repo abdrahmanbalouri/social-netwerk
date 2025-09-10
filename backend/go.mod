6617e87fa2fc1831d0f7e220549098805ef4b2f1
balouriabdrahman <balouriabdrahman@gmail.com>
2025-09-10 16:35:07 +0100

    end og login

--- backend/go.mod
+++ backend/go.mod
@@ -1,6 +1,6 @@
 module social-network
 
-go 1.23.0
+go 1.24.0
 
 toolchain go1.24.7
 
@@ -10,4 +10,8 @@ require (
 	github.com/rubenv/sql-migrate v1.8.0
 )
 
-require github.com/go-gorp/gorp/v3 v3.1.0 // indirect
+require (
+	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+)
--- backend/internal/api/route.go
+++ backend/internal/api/route.go
@@ -9,11 +9,12 @@ func Routes(db *sql.DB) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
 	})
 
-	mux.HandleFunc("/register", RegisterHandler)
-	// mux.HandleFunc("/login", LoginHandler(db))
+	mux.HandleFunc("/api/register", RegisterHandler)
+	mux.HandleFunc("/api/login", LoginHandler)
 	// mux.HandleFunc("/logout", LogoutHandler(db))
 
 	return mux
