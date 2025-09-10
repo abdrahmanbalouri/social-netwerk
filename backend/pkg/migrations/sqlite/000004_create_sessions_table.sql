7a869a7ffb4ac2410e09994c21eff83000c4daeb
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-09-10 12:52:53 +0100

    start

---
+++ backend/pkg/migrations/sqlite/000004_create_sessions_table.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    token TEXT NOT NULL UNIQUE,
+    expires_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS sessions;
