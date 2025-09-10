9b8900a0281ab99dfc9c68d2fc987a8cd89fd547
azraji <you@example.com>
2025-09-10 14:39:14 +0100

    starct

--- backend/internal/api/route.go
+++ backend/internal/api/route.go
@@ -9,10 +9,10 @@ func Routes(db *sql.DB) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Social Network API 🚀"))
+		
 	})
 
-	// mux.HandleFunc("/register", RegisterHandler(db))
+	mux.HandleFunc("/register", RegisterHandler)
 	// mux.HandleFunc("/login", LoginHandler(db))
 	// mux.HandleFunc("/logout", LogoutHandler(db))
 
