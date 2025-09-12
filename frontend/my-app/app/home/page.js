213e78afe222b398d45dad9e05944595e6d578fe
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-09-12 14:31:35 +0100

    hta

---
+++ frontend/my-app/app/home/page.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+
+export default function home() {
+  const router = useRouter();
+
+
+  async function logout(e) {
+    e.preventDefault();
+    const res = await fetch("http://localhost:8080/api/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    if (!res.ok) {
+      return;
+    }
+    router.replace("/login"); 
+  }
+
+
+  return (
+    <div>
+
+      <form onSubmit={logout}>
+        <button type="submit">Logout</button>
+      </form>
+    </div>
+  );
+}
