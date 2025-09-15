1e51ddd1028e4c8a0c67d543a29788d89beecb21
balouriabdrahman <balouriabdrahman@gmail.com>
2025-09-15 10:53:05 +0100

    split css file

--- frontend/my-app/app/layout.tsx
+++ frontend/my-app/app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
