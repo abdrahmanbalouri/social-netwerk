228736a3fc7c36af9ff5dd20b8d3a51d1013b91d
balouriabdrahman <balouriabdrahman@gmail.com>
2025-09-19 16:34:43 +0100

    just one

---
+++ backend/pkg/migrations/sqlite/000001_create_users_table.sql
@@ -0,0 +1,17 @@
+-- +migrate Up
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nickname TEXT DEFAULT "",
+    date_birth INTEGER NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    image TEXT NOT null ,
+    about TEXT NOT NULL ,
+    privacy TEXT NOT NULL ,
+    created_at INTEGER NOT NULL
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS users;
--- backend/pkg/migrations/sqlite/000008_create_groups_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    groups (
-        id TEXT PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        description TEXT,
-        admin_id INT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (admin_id) REFERENCES users (id)
-    )
