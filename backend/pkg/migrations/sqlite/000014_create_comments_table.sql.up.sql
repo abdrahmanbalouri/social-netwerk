971ec1f41dafbd81543a032041ec0e1e7bf96021
balouri <you@example.com>
2025-09-19 16:34:43 +0100

    just one

--- backend/pkg/migrations/sqlite/000014_create_comments_table.sql.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE comments (
-    id TEXT PRIMARY KEY,
-    post_id INT NOT NULL,
-    user_id INT NOT NULL,
-    content TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (post_id) REFERENCES posts(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
