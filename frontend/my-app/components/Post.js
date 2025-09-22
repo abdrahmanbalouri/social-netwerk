fa48d5dad995d6c1e21a8f26328afa98caf93ba6
Your Name <you@example.com>
2025-09-22 14:35:10 +0100

    hh

---
+++ frontend/my-app/components/Post.js
@@ -0,0 +1,37 @@
+// Post.js
+import Link from 'next/link';
+
+export default function Post({ post, onGetComments }) {
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profile ? `/Uploads/${post.profile}` : '/avatar.png'} alt="user" />
+            <div className="details">
+              <Link href={`/profile/${post.user_id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                <span className="name">{post.author}</span>
+              </Link>
+              <span className="date">{new Date(post.created_at).toLocaleString()}</span>
+            </div>
+          </div>
+        </div>
+        <div className="content">
+          <h3>
+            <p style={{ color: "#5271ff" }}>{post.title}</p>
+            {post.content}
+          </h3>
+          {post.image_path && <img src={post.image_path} alt="Post content" />}
+        </div>
+        <div className="info">
+          <div className="item">
+            <i className="fa-regular fa-heart"></i> {post.likes || 0} Likes
+          </div>
+          <div className="item" onClick={() => onGetComments(post)}>
+            <i className="fa-solid fa-comment"></i> {post.comments_count || 0} Comments
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
