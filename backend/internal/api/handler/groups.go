646213b43d6358b8641132e6f349b5c9f8ff4080
balouriabdrahman <balouriabdrahman@gmail.com>
2025-09-25 18:04:53 +0100

    25-09-2025

--- backend/internal/api/handler/groups.go
+++ backend/internal/api/handler/groups.go
@@ -7,35 +7,54 @@ import (
 	"strings"
 
 	"social-network/internal/helper"
+	"social-network/internal/repository"
 )
 
 func AddGroupHandler(w http.ResponseWriter, r *http.Request) {
+	// session, err := r.Cookie("session")
+	sessionID, err := r.Cookie("session_id")
+	fmt.Println("session iD in groups handler is :", sessionID)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"message":"unauthorized"}`))
+		return
+	}
+
 	type GroupRequest struct {
-		AdminID      string `json:"adminID"`
+		AdminID     string `json:"adminID"`
 		Title       string `json:"title"`
 		Description string `json:"description"`
 	}
-	if r.Method != http.MethodPost{
+	if r.Method != http.MethodPost {
 		helper.RespondWithError(w, http.StatusMethodNotAllowed, "Method not allowed")
 		return
 	}
 
 	var newGroup GroupRequest
 	if err := json.NewDecoder(r.Body).Decode(&newGroup); err != nil {
-		fmt.Println("111")
 		helper.RespondWithError(w, http.StatusBadRequest, "Invalid request format")
 		return
 	}
 
 	if len(strings.TrimSpace(newGroup.AdminID)) == 0 || len(strings.TrimSpace(newGroup.Title)) == 0 || len(strings.TrimSpace(newGroup.Description)) == 0 {
-		fmt.Println("2222")
 		helper.RespondWithError(w, http.StatusBadRequest, "Invalid request format")
 		return
 	}
 
-	query := `INSERT INTO groups (id, title, description, admin_id) VALUES (?, ?, ?, ?)`
-
+	// create new groups
 	grpID := helper.GenerateUUID()
-	fmt.Println("group id is :", grpID)
-	
+	query1 := `INSERT INTO groups (id, title, description, admin_id) VALUES (?, ?, ?, ?)`
+	_, err1 := repository.Db.Exec(query1, grpID, newGroup.Title, newGroup.Description, newGroup.AdminID)
+	if err1 != nil {
+		helper.RespondWithError(w, http.StatusInternalServerError, "Failed to create new group")
+		return
+	}
+
+	// insert the admin in the grp_members table
+	query2 := `INSERT INTO group_members (user_id, group_id) VALUES (?, ?)`
+	_, err2 := repository.Db.Exec(query2, newGroup.AdminID, grpID)
+	if err2 != nil {
+		helper.RespondWithError(w, http.StatusInternalServerError, "Failed to insert the admin in the group_members table")
+		return
+	}
 }
