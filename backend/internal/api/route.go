e4c70d2953bf3df668d0ef8d12a271aac1342fbb
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-09-28 00:05:02 +0100

    part 1 of group invitations

--- backend/internal/api/route.go
+++ backend/internal/api/route.go
@@ -14,20 +14,22 @@ func Routes(db *sql.DB) http.Handler {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusNotFound)
 	})
-   	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
+	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
 
 	mux.HandleFunc("/api/register", handlers.RegisterHandler)
 	mux.HandleFunc("/api/login", handlers.LoginHandler)
 	mux.HandleFunc("/api/logout", handlers.LogoutHandler)
 	mux.HandleFunc("/api/me", handlers.MeHandler)
-	mux.HandleFunc("/api/profile",handlers.ProfileHandler)
+	mux.HandleFunc("/api/profile", handlers.ProfileHandler)
 	mux.HandleFunc("/api/createpost", handlers.Createpost)
 	mux.HandleFunc("/api/Getpost/{id}", handlers.GetPostsHandler)
 	mux.HandleFunc("/api/Getallpost", handlers.AllpostsHandler)
-	mux.HandleFunc("/api/GetUsersHandler",handlers.GetUsersHandler)
-	mux.HandleFunc("/api/Getcomments/{id}",handlers.GetCommentsHandler)
-	mux.HandleFunc("/api/createcomment",handlers.CreateCommentHandler)
-	mux.HandleFunc("/api/groups",handlers.AddGroupHandler)
+	mux.HandleFunc("/api/GetUsersHandler", handlers.GetUsersHandler)
+	mux.HandleFunc("/api/Getcomments/{id}", handlers.GetCommentsHandler)
+	mux.HandleFunc("/api/createcomment", handlers.CreateCommentHandler)
+	mux.HandleFunc("/api/groups", handlers.AddGroupHandler)
+	mux.HandleFunc("/invitations/respond", handlers.GroupInvitationResponse)
+	mux.HandleFunc("/group/invitation", handlers.GroupInvitationRequest)
 
 	return mux
 }
