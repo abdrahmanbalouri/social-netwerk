27e4d388c23b46475d00ace38a0926212f05ff84
balouriabdrahman <balouriabdrahman@gmail.com>
2025-09-28 15:34:07 +0100

    add group id to post s table

--- backend/pkg/migrations/sqlite/20250923154618-create_posts_table.sql
+++ backend/pkg/migrations/sqlite/20250923154618-create_posts_table.sql
@@ -3,11 +3,13 @@
 CREATE TABLE IF NOT EXISTS posts (
     id TEXT PRIMARY KEY,
     user_id TEXT NOT NULL,
+    group_id TEXT DEFAULT NULL,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
     image_path TEXT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
 );
 -- +migrate Down
 DROP TABLE IF EXISTS posts;
