fe193d6df19ff85cb7ea7ba61d4bd670e6a71423
azraji <raji@gmail.com>
2025-10-09 15:47:56 +0100

    notef

---
+++ backend/internal/api/handler/Websocket.go
@@ -0,0 +1,71 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"social-network/internal/helper"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Message struct {
+	ReceiverId     string `json:"receiverId"`
+	MessageContent string `json:"messageContent"`
+	Type           string `json:"type"`
+}
+
+var ConnectedUsers = make(map[string][]*websocket.Conn)
+
+func Websocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Upgrade error:", err)
+		return
+	}
+	id, _ := helper.AuthenticateUser(r)
+	ConnectedUsers[id] = append(ConnectedUsers[id], conn)
+	defer conn.Close()
+	for {
+		_, msg, err := conn.NextReader()
+		if err != nil {
+			fmt.Println("Connection closed:", err)
+			break
+		}
+
+		var messageStruct Message
+		decoder := json.NewDecoder(msg)
+		err = decoder.Decode(&messageStruct)
+		if err != nil {
+			fmt.Println("err", err)
+			continue
+		}
+
+		if messageStruct.Type == "follow" {
+			messageStruct.ReceiverId = id
+		}
+
+		for i, conArr := range ConnectedUsers {
+			if i != id {
+				for _, con := range conArr {
+					jsonMsg, err := json.Marshal(messageStruct)
+					if err != nil {
+						fmt.Println("Error marshaling message:", err)
+						continue
+					}
+					if err := con.WriteMessage(websocket.TextMessage, jsonMsg); err != nil {
+						fmt.Println("Error writing message:", err)
+						break
+					}
+				}
+			}
+		}
+	}
+}
--- frontend/my-app/app/home/page.js
+++ frontend/my-app/app/home/page.js
@@ -11,7 +11,6 @@ import Stories from '../../components/stories.js';
 import Comment from '../../components/coment.js';
 import { useProfile } from '../../context/profile.js';
 import Post from '../../components/Post.js';
-import { middleware } from '../../midlwere/midle.js';
 import './Home.css';
 
 export default function Home() {
@@ -34,6 +33,7 @@ export default function Home() {
 
   const modalRef = useRef(null);
   const commentsModalRef = useRef(null);
+ 
   useEffect(() => {
 
     async function midle() {
@@ -94,18 +94,18 @@ export default function Home() {
       });
       const response = await res.json();
 
-      
-      if(res.ok){
+
+      if (res.ok) {
 
         const newpost = await fetchPosts(postId)
-        
 
 
-        
-  
+
+
+
         for (let i = 0; i < posts.length; i++) {
           if (posts[i].id == newpost.id) {
-  
+
             setPosts([
               ...posts.slice(0, i),
               newpost,
