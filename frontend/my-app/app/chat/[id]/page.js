ca6e95a1277ff5219cafaad308b45051118195e8
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-10-12 16:15:42 +0100

    Refactor chat components and styles; implement user fetching and messaging functionality

--- frontend/my-app/app/chat/[id]/page.js
+++ frontend/my-app/app/chat/[id]/page.js
@@ -1,23 +1,44 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Navbar from "../../../components/Navbar";
 import LeftBar from "../../../components/LeftBar";
-import RightBar from '../../../components/RightBar';
-import Navbar from '../../../components/Navbar';
-import { useDarkMode } from '../../../context/darkMod.js';
+import "../../../styles/chat.css";
+import UserBar from "../../../components/UserBar.js";
+import { useDarkMode } from "../../../context/darkMod.js";
+import ChatBox from "../../../components/chatBox.js";
 
-
-export default function chat() {
+export default function ChatPage() {
     const { darkMode } = useDarkMode();
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const res = await fetch(`http://localhost:8080/api/profile?userId=${id}`, {
+                    method: "GET",
+                    credentials: "include",
+                })
+                const data = await res.json();
+
+                setUser(data);
+            } catch (err) {
+                console.error("Error fetching user:", err);
+            }
+        }
+
+        fetchUser();
+    }, [id]);
 
     return (
         <div className={darkMode ? 'theme-dark' : 'theme-light'}>
-            <Navbar/>
+            <Navbar />
             <main className="content">
-            <div><LeftBar /></div>
-                    <h1>jmii</h1>
-                <div><RightBar /></div>
+                <LeftBar />
+                {user ? <ChatBox user={user} /> : <p className="loading">Loading user...</p>}
+                <UserBar />
             </main>
-
         </div>
-    )
+    );
 }
