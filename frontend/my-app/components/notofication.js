4ae1d6c049e68f6820fe3f6cf31fe49292dd22d2
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-10-13 12:44:29 +0100

    merge

--- frontend/my-app/components/notofication.js
+++ frontend/my-app/components/notofication.js
@@ -12,6 +12,7 @@ export default function Notification({ data }) {
                 <h3 className="notification-title">New notification</h3>
                 <i className="fa fa-times notification-close"></i>
             </div>
+            <Link href={`/profile/${data.receiverId}`} >
             <div className="notification-container">
                 <div className="notification-media">
                     <img src={data?.photo ? `/uploads/${data.photo}` : "/uploads/default.png"} alt="" className="notification-user-avatar" />
@@ -19,12 +20,13 @@ export default function Notification({ data }) {
                 </div>
                 <div className="notification-content">
                     <p className="notification-text">
-                        <strong>{data && data.name}</strong>,  {data && data.messageContent} 
+                        <strong>{data && data.name}</strong>,  {data && data.messageContent}
                     </p>
                     <span className="notification-timer">a few seconds ago</span>
                 </div>
                 <span className="notification-status"></span>
             </div>
-        </div>
+        </Link>
+        </div >
     );
 }
