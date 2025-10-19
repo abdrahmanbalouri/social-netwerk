02cbf43a2d8ba28f6301e96d9e57e1f9590f915b
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-10-20 00:04:18 +0100

    create post

--- frontend/my-app/app/groups/[id]/page.js
+++ frontend/my-app/app/groups/[id]/page.js
@@ -4,6 +4,7 @@ import Post from "../../../components/Post.js";
 import { useEffect, useState } from "react";
 import "./page.css"
 import { useParams } from "next/navigation";
+import { PostCreationTrigger } from "../../../components/cretaePostGroup.js"
 // import {CreatePost} from ""
 
 
@@ -12,12 +13,12 @@ export default function () {
         <>
             <Navbar />
             <AllPosts />
+            {/* <CreatePost /> */}
         </>
     )
 }
 
 function AllPosts() {
-
     const [posts, setPost] = useState([]);
     const [loading, setLoading] = useState(true);
     const params = useParams();
@@ -47,12 +48,18 @@ function AllPosts() {
             });
     }, [grpID]);
     if (!posts) {
-        return <div>There is no post yetttttt.</div>;
+        return (
+            <>
+                <PostCreationTrigger />
+                <div>There is no post yeeeeeet.</div>
+            </>
+        );
     }
+
     console.log("posts are :", posts);
     return (
         <>
-            {/* <CreatePost /> */}
+            <PostCreationTrigger />
             <div className="posts-list">
                 {posts.map((post) => (
                     <Post
@@ -76,4 +83,23 @@ function GetComments(post) {
 
 function AddLike() {
 
+}
+
+export async function CreatePost(groupId, formData) {
+    console.log("grouuup id is :", groupId);
+
+    const data = new FormData();
+    data.append("postData", JSON.stringify(formData)); 
+
+    const res = await fetch(`http://localhost:8080/group/addPost/${groupId}`, {
+        method: "POST",
+        credentials: "include",
+        body: data,
+    });
+
+    const text = await res.text();
+
+    if (!res.ok) throw new Error("Failed to create a post for groups");
+
+    return JSON.parse(text);
 }
