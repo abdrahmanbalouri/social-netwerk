60bf06b2c5a09d2c989d045f51ca527ac4ff0147
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-10-21 10:31:04 +0100

    create group_posts migration and update the backend

--- run.sh
+++ run.sh
@@ -18,3 +18,9 @@ npm run dev --scripts-prepend-node-path=true
 
 # BUT BEFORE INSTALL THIS :
 # go install github.com/rubenv/sql-migrate/sql-migrate@latest
+
+# IF YOU GET THIS ERROR : command not found: sql-migrate --- FOLLOW THESE STEPS 
+# 1- RUN THIS : go env GOPATH
+# 2- TAKE THE RESULT WJM3HA M3A HADO : /bin/sql-migrate 
+# 3- WRAHA ZID : new cretae_tableName
+# flkhr atkoun l command haka : /home/youruser/go/bin/sql-migrate new create_user_table -- ri l path li radi ttbdl 3la hssab chno l path dyalk
