e5c69e56f1da25ba02e407642626eb015e68d339
Marouane-EN <enmaroine443@gmail.com>
2025-10-29 20:41:18 +0100

    Refactor GroupCard component layout; add header and member count display

--- frontend/my-app/components/groupCard.js
+++ frontend/my-app/components/groupCard.js
@@ -1,15 +1,35 @@
-// import "../styles/groupstyle.css"
+import { Users, ChevronRight } from "lucide-react";
+
+
+
 export function GroupCard({ group, onShow }) {
-    console.log("inside groupCard function/component");
-    return (
-        <div className="group-card">
-            <div className="group-content">
-                <h2 className="group-title">{group.Title}</h2>
-                <p className="group-description">{group.Description}</p>
-            </div>
-            <div className="group-footer">
-                <button onClick={() => onShow(group.ID)}>Show</button>
-            </div>
+  console.log("inside groupCard function/component");
+  return (
+    <div className="group-card">
+      <div className="group-header">
+        <div className="group-icon-wrapper">
+          <div className="group-icon">
+            <Users />
+          </div>
         </div>
-    );
+      </div>
+      {/* Group Content */}
+      <div className="group-content">
+        <h2 className="group-title">{group.Title}</h2>
+        <p className="group-description">{group.Description}</p>
+        {/* Group Footer */}
+        <div className="group-footer">
+          <div className="group-members">
+            <Users />
+            <span className="members-text-full">{22} members</span>
+            <span className="members-text-short">{220}</span>
+          </div>
+          <button onClick={() => onShow(group.ID)}>
+            <span>View</span>
+            <ChevronRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
