53b292fa754c41fbf121d65e63d4e652c926e84e
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-10-30 09:17:19 +0100

    fix css for groups

--- frontend/my-app/components/groupPostCat.js
+++ frontend/my-app/components/groupPostCat.js
@@ -1,16 +1,16 @@
-import "../styles/groupstyle.css"
+// import "../styles/groupstyle.css"
 import {useState} from 'react'
-import "../styles/grouppage.css"
+// import "../styles/grouppage.css"
 import { AllPosts } from "../app/groups/[id]/page";
 
 export function GroupPostChat() {
     const [activeTab, setActiveTab] = useState('posts');
     return (
         <div className="main-container">
-            <div className='tabs-container '>
-                <button className={activeTab === 'posts' ? 'tab-button-group active' : 'tab-button-group'}
+            <div className='tabs'>
+                <button className={activeTab === 'posts' ? 'tab-buttonn active' : 'tab-buttonn'}
                     onClick={() => setActiveTab('posts')}>Posts</button>
-                <button className={activeTab === 'chat' ? 'tab-button-group active' : 'tab-button-group'}
+                <button className={activeTab === 'chat' ? 'tab-buttonn active' : 'tab-buttonn'}
                     onClick={() => setActiveTab('chat')}>Chat</button>
             </div>
 
