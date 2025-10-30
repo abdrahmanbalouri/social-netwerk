f7a29d1dd0579d36e172a52db3771a517948df72
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-10-30 13:02:06 +0100

    working on events

--- frontend/my-app/components/groupPostCat.js
+++ frontend/my-app/components/groupPostCat.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { AllPosts } from "../app/groups/[id]/page";
-import { FileText, MessageCircle } from "lucide-react";
+import {Events, AllPosts } from "../app/groups/[id]/page";
+import { Calendar, FileText, MessageCircle } from "lucide-react";
 
 export function GroupPostChat() {
   const [activeTab, setActiveTab] = useState("posts");
@@ -27,18 +27,24 @@ export function GroupPostChat() {
             <MessageCircle className="tab-icon" />
             <span>Chat</span>
           </button>
+           <button
+            className={`group-tab-button ${
+              activeTab === "event" ? "active" : "inactive"
+            }`}
+            onClick={() => setActiveTab("event")}
+          >
+            <Calendar className="tab-icon" />
+            <span>Events</span>
+          </button>
         </div>
 
         {/* <div className="group-container"> */}
-        {activeTab === "posts" ? (
+        {activeTab === "posts"  &&(
           <AllPosts />
-        ) : (
-          <div className="group-empty-state">
-            <MessageCircle className="tab-icon" />
-            <p className="group-empty-state-text">Chat interface coming soon</p>
-          </div>
-        )}
-        {/* </div> */}
+        ) }
+        {activeTab === "event" &&  (
+          <Events />
+        ) }
       </div>
     </div>
   );
