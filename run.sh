89d5737eb44688a2fabf398ab8dfe5013af77ddf
RedaAz07 <annizreda07@gmail.com>
2025-10-30 15:44:18 +0100

    db for events

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ npm run dev
 
 # IF YOU GET THIS ERROR : command not found: sql-migrate --- FOLLOW THESE STEPS 
 # 1- RUN THIS : go env GOPATH
-# 2- TAKE THE RESULT WJM3HA M3A HADO : /bin/sql-migrate 
+# 2- TAKE THE RESULT WJM3HA M3A HADO : /home/ranniz/go/bin/sql-migrate  new create_user_table
 # 3- WRAHA ZID : new cretae_tableName
 # flkhr atkoun l command haka : /home/youruser/go/bin/sql-migrate new create_user_table -- ri l path li radi ttbdl 3la hssab chno l path dyalk
