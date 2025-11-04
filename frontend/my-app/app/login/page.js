332c7fc6d26fcabe117a4fdfb6eb851d201b4d18
abdrahmanbalouri <balouriabdrahman@gmail.com>
2025-11-04 08:46:35 +0100

    add midlwere in frent

--- frontend/my-app/app/login/page.js
+++ frontend/my-app/app/login/page.js
@@ -1,39 +1,18 @@
 "use client"
-import { useState , useEffect } from "react";
-import { useRouter } from "next/navigation";
-import '../../styles/login.css';
+import { useState } from "react";
+import { useRouter } from "next/navigation"; // ← hna
+import './login.css';
+
 
 export default function Login() {
   const router = useRouter();
   const [form, setForm] = useState({ email: "", password: "" });
   const [err, setErr] = useState("");
-  const [loading, setLoading] = useState(true); 
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await fetch("http://localhost:8080/api/me", {
-          method: "GET",
-          credentials: "include", // mochkila hna 
-        });
-
-        if (res.ok) {
-          window.location.href = "/home";
-        } else {
-          setLoading(false);
-        }
-      } catch (error) {
-        setLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, []);
 
   async function submit(e) {
-    e.preventDefault();
-    setLoading(true);
     try {
+
+      e.preventDefault();
       const res = await fetch("http://localhost:8080/api/login", {
         method: "POST",
         credentials: "include",
@@ -41,59 +20,46 @@ export default function Login() {
         body: JSON.stringify(form),
       });
 
-      if (!res.ok) {
-        setErr(await res.text());
-        setLoading(false);
-        return;
-      }
 
+      if (!res.ok) return setErr(await res.text());
       window.location.href = "/home";
-    } catch (error) {
+    }catch (error) {
       setErr(error.message);
-      setLoading(false);
     }
   }
 
   return (
     <div className="container">
-      {loading ? (
-        <div className="loadingContainer">
-          <p>Loading...</p>
-        </div>
-      ) : (
-        <>
-          <div className="bg"></div>
-          <div className="loginCard">
-            <h1 className="title">Login</h1>
-            <form className="form" onSubmit={submit}>
-              <input
-                className="input"
-                placeholder="email"
-                value={form.email}
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-              />
-              <input
-                className="input"
-                type="password"
-                placeholder="password"
-                value={form.password}
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
-              />
-              <div className="buttonGroup">
-                <button className="loginButton" type="submit">Login</button>
-                <button
-                  type="button"
-                  className="registerButton"
-                  onClick={() => router.push("/register")}
-                >
-                  Register
-                </button>
-              </div>
-            </form>
-            {err && <p className="error">{err}</p>}
+      <div className="bg"></div>
+      <div className="loginCard">
+        <h1 className="title">Login</h1>
+        <form className="form" onSubmit={submit}>
+          <input
+            className="input"
+            placeholder="email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
+          <div className="buttonGroup">
+            <button className="loginButton" type="submit">Login</button>
+            <button
+              type="button"
+              className="registerButton"
+              onClick={() => router.push("/register")}
+            >
+              Register
+            </button>
           </div>
-        </>
-      )}
+        </form>
+        {err && <p className="error">{err}</p>}
+      </div>
     </div>
   );
 }
---
+++ frontend/my-app/middleware.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const API ="http://localhost:8080/";
+
+
+
+  try {
+    const response = await fetch(`${API}api/me`, {
+      headers: {
+        Cookie: request.headers.get('cookie') || ""
+      }
+    });
+    
+
+    const isAuth = response.ok;
+
+    if (!isAuth) {
+      if (pathname === "/login" || pathname === "/register") {
+        return NextResponse.next();
+      }
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+     const data = await response.json();
+     console.log(data);  
+    if (pathname === "/login" || pathname === "/register") {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+
+    return NextResponse.next();
+  } catch (err) {
+    console.error("Middleware Error:", err);
+    if (pathname === "/login" || pathname === "/register") {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    '/home',
+    '/login',
+    '/register',
+    '/chat/:path*',
+    '/chat',
+    '/profile/:path*',
+    '/groups/:path*',
+    '/games',
+    '/Gallery',
+    '/Follow/:path*',
+    '/Events',
+    '/Watch',
+    
+  ],
+};
