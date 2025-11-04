b02b807de63a6814cf630987d2482db8c467fc71
ussra-ch <yousra.ch.etudiante@gmail.com>
2025-11-04 05:18:03 +0100

    handle search

--- backend/internal/api/sevice/user_service.go
+++ backend/internal/api/sevice/user_service.go
@@ -1,14 +1,20 @@
 package service
 
 import (
-    "strings"
-    "social-network/internal/repository"
+	"database/sql"
+	"strings"
+
+	"social-network/internal/repository"
 )
 
 func SearchUsers(query string) ([]map[string]any, error) {
-    if query == "" {
-        return []map[string]any{}, nil
-    }
-    search := "%" + strings.ToLower(query) + "%"
-    return repository.SearchUsersInDB(repository.Db, search)
+	if query == "" {
+		return []map[string]any{}, nil
+	}
+	search := "%" + strings.ToLower(query) + "%"
+	return repository.SearchUsersInDB(repository.Db, search)
+}
+
+func GetUsers(db *sql.DB, currentUserID string) ([]repository.User, error) {
+	return repository.FetchAllUsers(db, currentUserID)
 }
