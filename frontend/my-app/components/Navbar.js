80f1bcda0075c50d290813d89b3dabe65daea921
abdrahmanbalouri <balouriabdrahman@gmail.com>
2025-11-06 10:43:45 +0100

    gg

--- frontend/my-app/components/Navbar.js
+++ frontend/my-app/components/Navbar.js
@@ -68,7 +68,7 @@ export default function Navbar() {
       setSearchResults([]);
       return;
     }
-
+  // clearTimeout(delay)    
     const delay = setTimeout(async () => {
       try {
         const res = await fetch(`http://localhost:8080/api/searchUser?query=${encodeURIComponent(searchTerm)}`, {
@@ -115,8 +115,6 @@ export default function Navbar() {
             placeholder="Search..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            onFocus={() => setShowResults(true)}
-            onBlur={() => setTimeout(() => setShowResults(false), 150)}
           />
 
           {showResults && Array.isArray(searchResults) && searchResults.length > 0 && (
